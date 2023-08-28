1a0619fb1cd71cc2c2074e4741c0aa03d0be53c6
MohammadAhmadShaaban <eng.mashaaban@yahoo.com>
2023-08-28 13:06:36 +0300

    frist

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication30.Models
+{
+    public class Category
+    {
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int CategoryID { get; set; }
+            public string CategoryName { get; set; }
+           
+        }
+
+    }
